9573d1f507c0500506af1bf88f295ed11f5f10e5
Elsayed Abdullah <sayedaladway@gmail.com>
2021-04-05 11:30:53 +0200

    add some edits to the video

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 $(function () {
   $("a.play").on("click", function (e) {
     e.preventDefault();
-    $(this).hide();
-    var video = '<iframe src="' + $(".play img").attr("data-video") + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    $(this).fadeOut();
+    var video = `
+    <video controls autoplay >
+      <source src="videos/save_earth.mp4" type="video/mp4" />
+    </video>`;
     $(".video-placeholder").replaceWith(video);
   });
 
-  $("#lp-video").prop("muted", true);
+  var $video = $("#lp-video")
+  $video.attr("controls", "");
+  $video.prop("muted", true);
   $(".volume-mute").on("click", function () {
     $(this).fadeOut();
-    if ($("#lp-video").prop("muted")) {
-      $("#lp-video").prop("muted", false);
+    if ($video.prop("muted")) {
+      $video.prop("muted", false);
+      $video.attr("controls", "");
     } else {
-      $("#lp-video").prop("muted", true);
+      $video.prop("muted", true);
     }
   });
+
 });
