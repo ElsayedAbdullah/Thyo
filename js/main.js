36602077dd4d05957e8473975c20365ab9095362
Elsayed Abdullah <sayedaladway@gmail.com>
2021-04-19 00:51:57 +0200

    add lazysizes and complete form validation for the checkout page

--- js/main.js
+++ js/main.js
@@ -9,17 +9,94 @@ $(function () {
     $(".video-placeholder").replaceWith(video);
   });
 
-  var $video = $("#lp-video")
-  $video.attr("controls", "");
-  $video.prop("muted", true);
-  $(".volume-mute").on("click", function () {
-    $(this).fadeOut();
-    if ($video.prop("muted")) {
-      $video.prop("muted", false);
-      $video.attr("controls", "");
+  var inputEmail = $("#email");
+  // function to validate the email
+  function validateEmail($email) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailReg.test($email);
+  }
+
+  var emailError = true;
+  var fNameError = true;
+  var lNameError = true;
+  var cityError = true;
+  var stateError = true;
+  var streetError = true;
+  var zipError = true;
+  var phoneError = true;
+  var cardError = true;
+  var cvcError = true;
+  var monthError = true;
+  var yearError = true;
+
+  inputEmail.on("blur", function () {
+    if (!validateEmail(inputEmail.val()) || inputEmail.val() == "") {
+      $(this).addClass("validate-error");
+      $(this).parent().addClass("not-validate").removeClass("validate");
+      emailError = true;
     } else {
-      $video.prop("muted", true);
+      $(this).removeClass("validate-error");
+      $(this).parent().addClass("validate").removeClass("not-validate");
+      emailError = false;
     }
   });
 
+  // validation for the select box
+  $("#state,#month,#year").on("blur", function () {
+    if (!$(this).val()) {
+      $(this).parents(".checkout_form").find(".select2-selection--single").addClass("validate-error");
+      $(this).parent().addClass("not-validate").removeClass("validate");
+      stateError = true;
+      monthError = true;
+      yearError = true;
+    } else {
+      $(this).parents(".checkout_form").find(".select2-selection--single").removeClass("validate-error");
+      $(this).parent().addClass("validate").removeClass("not-validate");
+      stateError = false;
+      monthError = false;
+      yearError = false;
+    }
+  });
+
+  $("#city,#st-address,#lname,#fname,#zip,#phone,#card_num,#card_cvc").on("blur", function () {
+    if ($(this).val() == "") {
+      $(this).addClass("validate-error");
+      $(this).parent().addClass("not-validate").removeClass("validate");
+      cityError = true;
+      streetError = true;
+      fNameError = true;
+      lNameError = true;
+      zipError = true;
+      phoneError = true;
+      cardError = true;
+      cvcError = true;
+    } else {
+      $(this).removeClass("validate-error");
+      $(this).parent().addClass("validate").removeClass("not-validate");
+      cityError = false;
+      streetError = false;
+      fNameError = false;
+      lNameError = false;
+      zipError = false;
+      phoneError = false;
+      cardError = false;
+      cvcError = false;
+    }
+  });
+
+  $(".checkout_form").on("submit", function (e) {
+    e.preventDefault();
+    if (emailError == true || fNameError == true || lNameError == true || cityError == true || stateError == true || streetError == true || zipError == true || phoneError == true || cardError == true || cvcError == true) {
+      $("select#state").blur();
+      $(inputEmail).blur();
+      $("#fname").blur();
+      $("#lname").blur();
+      $("#city").blur();
+      $("#st-address").blur();
+      $("#zip").blur();
+      $("#phone").blur();
+      $("#card_num").blur();
+      $("#card_cvc").blur();
+    }
+  });
 });
